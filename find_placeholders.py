5eb7042f7c41ec9d83003d6a0a1b187997ffaf31
Shoham Peller <shohamp@gmail.com>
2015-05-04 14:02:18 +0300

    ident with spaces, not tabs (according to PEP8)

--- find_placeholders.py
+++ find_placeholders.py
@@ -1,33 +1,33 @@
 import re
 
 def find_placeholders_in_format_string(format_string):
-	"""
-	Find inside a C-style format string, all the the placeholders for parameters
-	i.e. for "Worker name is %s and id is %d" will return the indexes and the placeholders:
-	[(15, "%s"), (28, "%d")]
-	"""
+    """
+    Find inside a C-style format string, all the the placeholders for parameters
+    i.e. for "Worker name is %s and id is %d" will return the indexes and the placeholders:
+    [(15, "%s"), (28, "%d")]
+    """
 
-	# According to printf format string specification from Wikipedia, the format is:
-	# %[parameter][flags][width][.precision][length]type
-	# see http://en.wikipedia.org/wiki/Printf_format_string#Format_placeholders
-	PLACEHOLDERS_REGEX = """
-	(										# Capture everything in group 1
-	%										# Look for a literal '%'
-	(?:
-	(?:\d+\$)?								# Look for an optional paramter - number and then '$'
-	[-+ 0#]{0,5}							# 0 to 5 optional flags
-	(?:\d+|\*)?								# Optional width
-	(?:\.(?:\d+|\*))?						# Optional precision
-	(?:hh|h|l|ll|L|z|j|t|q|I|I32|I64|w)?	# Optional length
-	[diufFeEgGxXoscqaAn]					# Type
-	|										# OR
-	%))										# literal "%%"
-	"""
+    # According to printf format string specification from Wikipedia, the format is:
+    # %[parameter][flags][width][.precision][length]type
+    # see http://en.wikipedia.org/wiki/Printf_format_string#Format_placeholders
+    PLACEHOLDERS_REGEX = """
+    (                                       # Capture everything in group 1
+    %                                       # Look for a literal '%'
+    (?:                                     # Match an actual placeholder (and not '%%')
+    (?:\d+\$)?                              # Look for an optional paramter - number and then '$'
+    [-+ 0#]{0,5}                            # 0 to 5 optional flags
+    (?:\d+|\*)?                             # Optional width
+    (?:\.(?:\d+|\*))?                       # Optional precision
+    (?:hh|h|l|ll|L|z|j|t|q|I|I32|I64|w)?    # Optional length
+    [diufFeEgGxXoscqaAn]                    # Type
+    |                                       # OR
+    %))                                     # literal "%%"
+    """
 
-	matches = re.finditer(PLACEHOLDERS_REGEX, format_string, flags=re.X)
-	results = []
-	for i in matches:
-		# Remove all the "%%" found. These are literal "%", not placeholders
-		if i.group(0) != "%%":
-			results.append((i.start(0), i.group(0)))
-	return results
+    matches = re.finditer(PLACEHOLDERS_REGEX, format_string, flags=re.X)
+    results = []
+    for i in matches:
+        # Remove all the "%%" found. These are literal "%", not placeholders
+        if i.group(0) != "%%":
+            results.append((i.start(0), i.group(0)))
+    return results
--- tests/test_find_placeholders.py
+++ tests/test_find_placeholders.py
@@ -9,26 +9,26 @@ sys.path.insert(0, os.path.join(SCRIPT_DIR, ".."))
 from find_placeholders import find_placeholders_in_format_string
 
 class TestFindPlaceholders(unittest.TestCase):
-	def test_placeholders(self):
-		str_to_test = """
-							Worker name is %s and id is %d
-							That is %i%%i
-							%c
-							Decimal: %d  Justified: %.6d
-							%10c%5hc%5C%5lc
-							The temp is %.*f
-							%ss%lii
-							%*.*s | %.3d | %lC | %s%%%02d
-					  """
+    def test_placeholders(self):
+        str_to_test = """
+                            Worker name is %s and id is %d
+                            That is %i%%i
+                            %c
+                            Decimal: %d  Justified: %.6d
+                            %10c%5hc%5C%5lc
+                            The temp is %.*f
+                            %ss%lii
+                            %*.*s | %.3d | %lC | %s%%%02d
+                      """
 
-		expected_results = [(23, '%s'), (36, '%d'), (54, '%i'), (67, '%c'), (86, '%d'),	\
-						    (101, '%.6d'), (113, '%10c'), (117, '%5hc'), (124, '%5lc'), \
-						    (148, '%.*f'), (160, '%s'), (163, '%li'), (175, '%*.*s'), 	\
-						    (183, '%.3d'), (196, '%s'), (200, '%02d')]
+        expected_results = [(23, '%s'), (36, '%d'), (54, '%i'), (67, '%c'), (86, '%d'),    \
+                            (101, '%.6d'), (113, '%10c'), (117, '%5hc'), (124, '%5lc'), \
+                            (148, '%.*f'), (160, '%s'), (163, '%li'), (175, '%*.*s'),     \
+                            (183, '%.3d'), (196, '%s'), (200, '%02d')]
 
-		func_result = find_placeholders_in_format_string(str_to_test)
+        func_result = find_placeholders_in_format_string(str_to_test)
 
-		self.assertEqual(func_result, expected_results)
+        self.assertEqual(func_result, expected_results)
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
